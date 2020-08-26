55970329729de530f511d88a21cad56618e75394
Cloe Furlan <cloe.furlan@yahoo.fr>
2020-08-26 14:10:58 -0400

    Back end for recipes#new

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,10 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :meals, dependent: :destroy
   has_one_attached :photo
+  validates :title, presence: true, uniqueness: true
+  validates :ingredients, presence: true
+  validates :instructions, presence: true
+  validates :serving, presence: true
+  validates :preptime, presence: true
+  validates :difficulty, presence: true, inclusion: { in: ["Easy", "Intermediate", "Advanced"] }
 end
