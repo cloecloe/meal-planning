c07cfeaddd8d5ada52da957e3165b7b19d92a675
Rebecca Morel <rebecca.morel@mail.mcgill.ca>
2020-08-26 16:26:08 -0400

    link to filters on recipes_index and working

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
   def index
     @recipes = Recipe.where(displayed: true)
+    filter(params[:format])
   end
 
   def new
@@ -20,4 +21,31 @@ class RecipesController < ApplicationController
 
   def destroy
   end
+
+  def filter(tag)
+    case tag
+    when "easy"
+      @recipes = Recipe.where(difficulty: "Easy", displayed: true)
+    when "effort"
+      @recipes = Recipe.where(difficulty: "More effort", displayed: true)
+    when "challenge"
+      @recipes = Recipe.where(difficulty: "A challenge", displayed: true)
+    when "quick"
+      @recipes = Recipe.where(preptime: (10..30), displayed: true)
+    when "short"
+      @recipes = Recipe.where(preptime: (31..59), displayed: true)
+    when "long"
+      @recipes = Recipe.where(preptime: (1..5), displayed: true)
+    when "indivudual"
+      @recipes = Recipe.where(serving: 0 || 1, displayed: true)
+    when "couple"
+      @recipes = Recipe.where(serving: 2, displayed: true)
+    when "family"
+      @recipes = Recipe.where(serving: (3..100), displayed: true)
+    else ""
+      @recipes = Recipe.where(displayed: true)
+    end
+    render :index
+  end
+  
 end
