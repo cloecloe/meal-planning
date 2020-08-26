646064c52031666594cee9e34f411878664f4046
Cloe Furlan <cloe.furlan@yahoo.fr>
2020-08-26 11:24:15 -0400

    Added Difficulty to Recipe model and seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_190805) do
+ActiveRecord::Schema.define(version: 2020_08_26_150149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2020_08_25_190805) do
     t.integer "preptime"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "difficulty"
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
