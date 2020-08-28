aeaa747874f83871d1113d1d897953554edb63fa
Cloe Furlan <cloe.furlan@yahoo.fr>
2020-08-28 10:15:13 -0400

    Added icons to index pages

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -5,11 +5,39 @@ class MealsController < ApplicationController
     @meals = Meal.all
     @array = []
   end
-  
+
   def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @meal = Meal.new
+
+
   end
+
+
+
+  # def create
+  #   @recipe = Recipe.find(params[:recipe_id])
+  #   @favorite = Favorite.new
+
+  #   if params[:favorite][:favorited] == "true"
+  #     @favorite_current = Favorite.where(user_id: current_user, recipe_id: params[:recipe_id])
+  #     @favorite_current.first.destroy
+  #   else
+  #     Favorite.create(user: current_user, recipe_id: params[:recipe_id])
+  #   end
+  #   redirect_to recipe_path(@recipe)
+  # end
+
+
+
 
   def destroy
     Meal.destroy(params[:meal_ids])
+  end
+
+  private
+
+  def meal_params
+    params.require(:meal).permit(:tagname, :date)
   end
 end
