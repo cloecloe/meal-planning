d4cfa56566c7be3c010ed979546bfa5946e934ac
Rebecca Morel <rebecca.morel@mail.mcgill.ca>
2020-08-31 14:58:10 -0400

    8 filters in carousel finished

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -87,16 +87,12 @@ class RecipesController < ApplicationController
       @recipes = Recipe.where(difficulty: "Easy", displayed: true)
     when "effort"
       @recipes = Recipe.where(difficulty: "More effort", displayed: true)
-    when "challenge"
-      @recipes = Recipe.where(difficulty: "A challenge", displayed: true)
     when "quick"
       @recipes = Recipe.where(preptime: (10..30), displayed: true)
     when "short"
       @recipes = Recipe.where(preptime: (31..59), displayed: true)
     when "long"
       @recipes = Recipe.where(preptime: (1..5), displayed: true)
-    when "individual"
-      @recipes = Recipe.where(serving: "Serves 1", displayed: true)
     when "couple"
       @recipes = Recipe.where(serving: "Serves 2", displayed: true)
     when "family"
