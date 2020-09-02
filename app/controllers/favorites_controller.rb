f486350935a80829a678aae1b2c6491a214fb482
Emily Rockarts <em.rockarts@gmail.com>
2020-09-02 12:29:41 -0400

    empty cookbook recipes and responsiveness

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,6 +2,9 @@ class FavoritesController < ApplicationController
   def index
     @favorites = Favorite.includes(:recipe).where(user: current_user)
     policy_scope(Favorite)
+    @other_recipe = Recipe.all.sample
+    @second_recipe = Recipe.select { |recipe| recipe.id != @other_recipe.id }.sample
+    @third_recipe = Recipe.select { |recipe| recipe.id != @second_recipe.id && recipe.id != @other_recipe.id }.sample
   end
 
   def create
